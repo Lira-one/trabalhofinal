5c4dc3906e80ee57d3436b8dc6cf9fe09a7e6eaf
Lira-one <gabriellira413@gmail.com>
2025-06-03 19:51:04 -0300

    Update kabum.py

--- AP2_WEBDATA_KABUM.zip/script/kabum.py
+++ AP2_WEBDATA_KABUM.zip/script/kabum.py
@@ -13,7 +13,7 @@ Aqui, vamos analisar valores nulos, estatísticas descritivas e relações entre
 # 2. Carregar o dataset
 st.header("📂 1. Carregamento dos Dados")
 
-caminho_csv = "dados_kabum2.csv"
+caminho_csv = "AP2_WEBDATA_KABUM.zip/script/kabum.py"
 
 try:
     df = pd.read_csv(caminho_csv, sep=';')
